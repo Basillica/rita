df038e5f6110f2f200783ab0cfb064b66cfcc770
anthony <ezeabasilianthony@gmail.com>
2021-08-15 22:22:33 +0200

    refactored plotter

--- visuals/plot.py
+++ visuals/plot.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('WebAgg')
 
-tr_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-tr_data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# tr_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# tr_data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
-def plot():
+def plot(tr_data):
     fig1 = plt.figure()
     plt.xlabel('RelativeTime (ms)')
     plt.ylabel('AbsoluteTime (ms)')
     plt.title('R-A Combinations')
-    plt.plot(tr_data[:], tr_data2[:], 'bx')
+    plt.plot(tr_data[:, 1], tr_data[:, 2], 'bx')
     # plt.plot(tr_data[outliers,0],tr_data[outliers,1],'ro')
     value = mpld3.fig_to_html(fig1, d3_url=None, mpld3_url=None, no_extras=False,
                               template_type='general', figid=None, use_http=False)
